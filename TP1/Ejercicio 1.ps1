288bb00f1bd08428094a77fa85ff7b12c76813e9
Kampeador2047 <nahuel.ambroso@gmail.com>
2016-09-03 19:58:12 -0300

    Ejercicio1
    Falta que pongan bien sus nombres y apellidos, en la parte que dice
    entrega poner la fecha de la entrega. despues

---
+++ TP1/Ejercicio 1.ps1
@@ -0,0 +1,58 @@
+﻿<#
+Nombre del script: Ejercicio1.ps1
+Trabajo práctico número 1
+Ejercicio 1
+
+Ambroso, Nahuel Oscar   DNI:34.575.684
+Currao, Martin          DNI:
+Sebastian,		DNI:
+Juan Cruz,		DNI:
+Gabriel, Rodriguez	DNI:
+
+Entrega
+#>
+
+Param($pathsalida)  #define que el parámetro ingresado se llame pathsalida 
+
+$existe = Test-Path $pathsalida  #hace la validación del parametro pathsalida
+if ($existe -eq $true)  #evalúa si es verdadera la validación del path.
+{
+    $lista = Get-ChildItem -File  #Toma una lista de archivos del directorio actual 
+    
+    foreach ($item in $lista)
+    {
+        Write-Host "$($item.Name) $($item.Length)" #Evalúa cada item en la lista dada, mostrando por pantalla nombre y tamaño
+    }
+}
+else
+{
+    write-Error "El path no existe"  #Imprime en el caso de no ser válido el path, que el mismo no existe.
+}
+
+<#
+
+A:
+
+El objetivo del script es tomar un path pasado por parámetro, 
+validar si es correcto, de no serlo se procede a imprimir 
+por pantalla que no existe el path ingresado, en el caso de ser válido
+se lista en pantalla el nombre y el tamaño en bits de todos aquellos 
+archivos que se encuentran dentro del directorio donde se encuentra el script.
+
+B:
+
+Agregaría una validación para que exija ingresar un path obligatoriamente
+Ej: param(
+            [$pathsalida (position = 1, Mandatory = $true)]
+         )
+
+
+c:
+
+La misma podría ser reemplazada por:
+
+get-childitem -File 
+
+puesto que no utiliza el path ingresado para extraer la lista, la extrae del directorio donde se ejecuta el script.
+
+#>
