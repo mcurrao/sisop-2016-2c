a399e29aabef2f7befc3ade5e6507430f83e6643
Martin Currao <mcurrao@gmail.com>
2016-09-24 18:59:57 -0300

    Append de lineas
    Se agrega el append de lineas cuando no exista esa configuracion ya en el archivo. Validaciones varias. Quedan pendientes los parámetros opcionales (los del tipo -x)
    Tambien queda pendiente reemplazar una configuracion existente.

--- TP2/Configurar.sh
+++ TP2/Configurar.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 if [ "$#" -lt 3 ]; then
-    echo "Se deben pasar al menos 3 parametros"
+    echo "Uso: Configurar.sh archivo clave valor [-y][-c comentario]"
     exit
 fi
 if [ ! -f "$1" ] ; then
     echo "$1 no es un archivo";
     exit;
 fi
+if [ ! -w "$1" ] ; then
+    echo "No se tienen permisos de escritura sobre $1";
+    exit;
+fi
 confirmation=0
+comment=
 while getopts ":yc:" opt; do
   case $opt in
     y)
@@ -25,3 +30,16 @@ while getopts ":yc:" opt; do
       ;;
   esac
 done
+addedBy=`whoami`
+addedAt=`date`
+foundInFile=`grep -x -c -m 1 "$2=.*" $1`
+echo F : $foundInFile
+if [ "$foundInFile" -eq 0 ] ; then
+echo $comment
+#La linea de debajo no esta comentada, es para documentar
+cat <<EOT >> $1
+# Agregado por $addedBy el $addedAt - $comment
+$2=$3
+EOT
+echo "Se agrega $2=$3 al archivo"
+fi
